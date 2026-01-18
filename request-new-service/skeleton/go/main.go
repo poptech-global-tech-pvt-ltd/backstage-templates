d8c395944824178ea945113100cd696564be70a3
Rutvik <rutvik@popclub.co>
2026-01-18 13:51:13 +0530

    create service

--- request-new-service/skeleton/go/main.go
+++ request-new-service/skeleton/go/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello from DevOps Team!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from DevOps Team! Your new Go service is ready to be developed.")
+	})
+	fmt.Println("Server running on port 8000")
+	http.ListenAndServe(":8000", nil)
 }
