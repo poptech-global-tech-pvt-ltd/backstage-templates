8fb123e78bf748dd0363a7a50b0ee38e9c28ff38
Rutvik <rutvik@popclub.co>
2026-01-19 14:39:16 +0530

    health go

--- request-new-service/skeleton/go/repository/main.go
+++ request-new-service/skeleton/go/repository/main.go
@@ -9,6 +9,10 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello from DevOps Team! Your new Go service is ready to be developed.")
 	})
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK")
+	})
 	fmt.Println("Server running on port 8000")
 	http.ListenAndServe(":8000", nil)
 }
